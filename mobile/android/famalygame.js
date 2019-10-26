d2eacc52ed65b5d2a441fb7b328df2dae4953f54
yorikya@gmail.com <yorikya@gmail.com>
2019-10-26 18:56:29 +0300

    Cleaning code in android app

--- mobile/android/famalygame.js
+++ mobile/android/famalygame.js
@@ -1,74 +1,83 @@
 var _serverhost = "http://10.0.0.2:8080"
 var _client_data;
-var _app_fldr = "/sdcard/DroidScript"
+var _app_fldr = "/sdcard/DroidScript";
 var _app_login_lay;
 var _app_chat_lay;
 
 function createLoginLayout() {
       //Create the login page.
-	lay = app.CreateLayout( "Linear", "FillXY" );	
+	let lay = app.CreateLayout( "Linear", "FillXY" );	
 	lay.SetBackground( "/Sys/Img/BlueBack.jpg" );
 	lay.SetPadding( 0, 0.1, 0, 0 ); 
 
-    username = app.CreateText( "User Name" , 0.3, 0.04);
-    username.SetMargins(-0.2,0,0,0)
-    username.SetTextSize( 20 );
-    lay.AddChild( username );
+    let login_userid_lbl = app.CreateText( "User Name" , 0.3, 0.04);
+    login_userid_lbl.SetMargins(-0.2,0,0,0);
+    login_userid_lbl.SetTextSize( 20 );
+    lay.AddChild( login_userid_lbl );
     
-	userID = app.CreateTextEdit( "yuri", 0.7, 0.1 );
-	userID.SetMargins(0,0,0,0.1)
-    lay.AddChild( userID );
+	login_userid_edt = app.CreateTextEdit( "yuri", 0.7, 0.1 );
+	login_userid_edt.SetMargins(0,0,0,0.1)
+    lay.AddChild( login_userid_edt );
     
-    passlable = app.CreateText( "Password" , 0.3, 0.04);
-    passlable.SetMargins(-0.2,0,0,0)
-    passlable.SetTextSize( 20 );
-    lay.AddChild( passlable );
+    let login_pass_lbl = app.CreateText( "Password" , 0.3, 0.04);
+    login_pass_lbl.SetMargins(-0.2,0,0,0)
+    login_pass_lbl.SetTextSize( 20 );
+    lay.AddChild( login_pass_lbl );
     
-    password = app.CreateTextEdit( "a", 0.7, 0.1);
-    lay.AddChild( password );
+    login_pass_edt = app.CreateTextEdit( "a", 0.7, 0.1);
+    lay.AddChild( login_pass_edt );
     
     //Create button and add to main layout.
-	loginBtn = app.CreateButton( "Login", 0.4, 0.1, "gray" );
-	loginBtn.SetMargins(0,0.1,0,0)
-	loginBtn.SetOnTouch( function(){sendLoginRequest(userID.GetText(), password.GetText());});
-	lay.AddChild( loginBtn );
-	return lay
+	let login_btn = app.CreateButton( "Login", 0.4, 0.1, "gray" );
+	login_btn.SetMargins(0,0.1,0,0)
+	login_btn.SetOnTouch( function(){sendLoginRequest(login_userid_edt.GetText(), login_pass_edt.GetText());});
+	lay.AddChild( login_btn );
+	
+	return lay;
 }
 
 function createChatLayout() {
-	lay = app.CreateLayout( "Linear", "FillXY" );
+	let lay = app.CreateLayout( "Linear", "FillXY" );
 	lay.SetPadding( 0, 0.1, 0, 0 ); 
 	lay.SetBackground( "/Sys/Img/GreenBack.jpg" );
 	lay.SetVisibility( "Hide" );
 	
 	//Create button and add to sliding layout.
-	btnBack = app.CreateButton( "Back", 0.3, 0.06, "gray" );
-	btnBack.SetOnTouch( btnBack_OnTouch );
-	lay.AddChild( btnBack );
+	chat_back_btn = app.CreateButton( "Back", 0.3, 0.06, "gray" );
+	chat_back_btn.SetOnTouch(function(){ _app_chat_lay.Animate( "SlideToLeft" ); });
+	lay.AddChild( chat_back_btn );
 	
-	txt = app.CreateList("Conversations", 0.8, 0.4  );
-	lay.AddChild( txt );
-	//Create a button 1/3 of screen width and 1/10 screen height.
-	btn = app.CreateButton( "Press Me", 0.3, 0.1 );
-	btn.SetMargins( 0, 0.05, 0, 0 );
+	chat_converse_lst = app.CreateList("Conversations", 0.8, 0.4  );
+	lay.AddChild( chat_converse_lst );
 	
-	//Set function to call when button pressed.
-	btn.SetOnTouch( btn_OnTouch );
-	lay.AddChild( btn );
+	chat_msg_send_lay = app.CreateLayout( "Linear", "Horizontal,FillXY" );
 	
-	edt = app.CreateTextEdit( "Hello", 0.7, 0.1, "Multiline" );
-    edt.SetTextColor( "#ff6666ff" );
-    edt.SetBackColor( "#ffffffff" );
-    edt.SetOnChange(function() { 
-        app.Debug( edt.GetText() );
+	chat_msg_edt = app.CreateTextEdit( "Hello", 0.75, 0.07, "Multiline" );
+    chat_msg_edt.SetTextColor( "#ff6666ff" );
+    chat_msg_edt.SetBackColor( "#ffffffff" );
+    chat_msg_edt.SetOnChange(function() { 
+        let t = chat_msg_edt.GetText();
+        let last_char = t.substring(t.length-1, t.length);
+        if (last_char == "\n") {
+            sendMessage(t.replace(/\n/g, ''));
+            chat_msg_edt.SetText("");  
+            chat_converse_lst.ScrollToItemByIndex(chat_converse_lst.GetLength());
+        }
+        
     });
-    lay.AddChild( edt );
+    chat_msg_send_lay.AddChild(chat_msg_edt);
     
-    //Create a text label and add it to layout.
-	txtMsg = app.CreateText( "", 0.8, 0.3, "AutoScale,MultiLine" );
-	txtMsg.SetTextSize( 22 );
-	lay.AddChild( txtMsg );
-	return lay
+	let chat_send_btn = app.CreateButton( "Send", 0.2, 0.07 );
+	chat_send_btn.SetMargins( 0.015, 0, 0, 0 );
+	chat_send_btn.SetOnTouch(function(){ 
+	    sendMessage(chat_msg_edt.GetText());
+        chat_msg_edt.SetText(""); 
+    });    
+	chat_msg_send_lay.AddChild( chat_send_btn );
+	
+	lay.AddChild(chat_msg_send_lay)
+	
+	return lay;
 }
 
 //Main: Called when application is started.
@@ -87,7 +96,6 @@ function OnStart() {
 	app.AddLayout( _app_login_lay );
 	app.AddLayout( _app_chat_lay );
 	
-// 	setInterval( ShowServerResource, 1000 );
 }
 
 function sendLoginRequest(user, pass) {
@@ -98,13 +106,13 @@ function sendLoginRequest(user, pass) {
     {
         if( error ) alert( error );
         else {
-            r = JSON.parse(reply);
+            let r = JSON.parse(reply);
             alert(reply);
             if (r.success == true) {
                 _client_data = r;
                 let urls = [];
                 _client_data.filenames.forEach(function (name, _) {
-                    urls.push(_serverhost+_client_data.resources+name)
+                    urls.push(_serverhost+_client_data.resources+name);
                 });
 
                 let fldr = _app_fldr + _client_data.resources;
@@ -112,12 +120,13 @@ function sendLoginRequest(user, pass) {
                 app.MakeFolder( fldr );
                 
                 dload = app.CreateDownloader(/*"NoDialog"*/  );
-                dload.SetOnDownload( function(file) { _app_chat_lay.Animate( "SlideFromLeft" );});
                 dload.SetOnError( function(error) { alert(error);});
                 dload.Download( urls, fldr );
-                            
+                
+                _app_chat_lay.Animate( "SlideFromLeft" );
+                
                 } else {
-                    alert( r.error)
+                    alert( r.error);
                 }
                 
             }
@@ -127,14 +136,9 @@ function sendLoginRequest(user, pass) {
 //Handle servlet requests. 
 function onServlet( request, info ) { 
 	serv.SetResponse( "Got it!" ); 
-// 	txt = app.CreateList("Conversations", 0.8, 0.4  );
-    txt.AddItem(request.id+ ": "+ request.msg);
+    chat_converse_lst.AddItem(request.id, request.msg + "\n\n[10:52]");
+    chat_converse_lst.ScrollToItemByIndex(chat_converse_lst.GetLength());
 } 
-
-//Called when user touches our back button.
-function btnBack_OnTouch() {
-	_app_chat_lay.Animate( "SlideToLeft" );	
-}
 
 function sendMessage(message) {
       //Send request to remote server.
@@ -143,10 +147,4 @@ function sendMessage(message) {
     app.HttpRequest( "get", _serverhost, path, params, function ( error, reply ) {
         if( error ) alert( error );        
     });   
-}
-
-//Called when user touches our button.
-function btn_OnTouch() {
-    sendMessage(edt.GetText());
-    edt.SetText("");
 }
